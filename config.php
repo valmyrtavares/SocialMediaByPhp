a20e394315f699de531c4e244dc4cd2e7fdce2e4
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-07-16 17:49:05 -0300

    First One

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$base = 'http://localhost/devsbook'; 
+
+$db_name = 'devsbook';
+$db_host = 'locahost';
+$db_user = 'root';
+$db_pass = 'Amanda@230406';
+
+$pdo = new PDO("mysql:dbname =".$db_name.";host = ".$db_host,$db_user, $db_pass);
