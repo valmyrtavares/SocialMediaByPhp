f3bd7d1c1b421e4c6c69a6e4dc8f2c7e6697c767
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-07-25 23:24:52 -0300

    view photos

--- perfil.php
+++ perfil.php
@@ -157,12 +157,12 @@ $feed = $postDao->getUserFeed($id);
                 </div>
                 <div class="box-body row m-20">
                     <?php if(count($user->photos) > 0): ?>
-                        <?php foreach($user->photos as $item):?>
+                        <?php foreach($user->photos as $key=> $item):?>
                             <div class="user-photo-item">
-                                <a href="#modal-1" rel="modal:open">
+                                <a href="#modal-<?= $key ?>" rel="modal:open">
                                     <img src="<?=$base; ?>/media/uploads/<?=$item->body; ?>" />
                                 </a>
-                                <div id="modal-1" style="display:none">
+                                <div id="modal-<?= $key ?>" style="display:none">
                                     <img src="<?=$base; ?>/media/uploads/<?=$item->body; ?>" />
                                 </div>
                             </div>
